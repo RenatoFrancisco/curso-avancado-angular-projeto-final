fb2615b2f83e462263f64c5eaa199f2a1aa948de
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-11-05 16:55:17 -0300

    Estrutura do módulo de autenticação

---
+++ front-end/src/app/conta/conta.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+
+import { ContaRoutingModule } from './conta.route';
+import { ContaAppComponent } from './conta.app.component';
+
+
+
+@NgModule({
+  declarations: [
+    CadastroComponent,
+    LoginComponent,
+    ContaAppComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ContaRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ]
+})
+export class ContaModule { }
